696adcf2b6e66c0a8fbf14b290feec57c9e5af6c
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-04-18 12:58:02 -0400

    Update replacingingredients.py
    adding ingredients to change

--- ingredients/replacingingredients.py
+++ ingredients/replacingingredients.py
@@ -50,7 +50,20 @@ def perform_replacements(row):
         elif ner_list[i] == 'oleo':
             ner_list[i] = 'margarine'
 
-    
+        elif ner_list[i] == 'soda':
+            ner_list[i] = 'baking soda'
+
+        elif ner_list[i] == 'chicken breasts':
+            ner_list[i] = 'chicken'
+
+        elif ner_list[i] == 'whipping cream':
+            ner_list[i] = 'heavy cream'
+
+        elif ner_list[i] == 'soda':
+            ner_list[i] = 'baking soda'
+
+        
+
     
     # Convert list back to string and update 'NER' column in row
     row['NER'] = str(ner_list)
