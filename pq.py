7d146efb5fdaea2905d0f60788112b637560183a
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-04-21 16:39:53 -0400

    updated pq.py for comments

--- pq.py
+++ pq.py
@@ -3,54 +3,57 @@ import tkinter as tk
 
 class PriorityQueue:
     def __init__(self):
+        # dictionary to store recipes before inserting into priority queue
         self.dict = {}
         self.queue = []
         
+    # used in main to insert into dictionary
     def dict_insert(self, recipename, recipeingredientlist, recipelink, priority):
         if recipename not in self.dict:
             self.dict[recipename] = (recipeingredientlist, recipelink, priority)
     
     def pq_insert(self, recipename, recipeingredientlist, recipelink, priority):
         self.queue.append((recipename, recipeingredientlist, recipelink, priority))
-        self._heapify_up(len(self.queue) - 1)
+        self.heapify_up(len(self.queue) - 1)
     
     def pop(self):
         if not self.queue:
             return None
         max_item = self.queue[0]
-        last_item = self.queue.pop()
-        if self.queue:
-            self.queue[0] = last_item
-            self._heapify_down(0)
+        self.queue[0] = self.queue[-1]
+        self.queue.pop()
+        self.heapify_down(0)
         return max_item
     
-    def _heapify_up(self, index):
-        while index > 0:
-            parent_index = (index - 1) // 2
-            if self.queue[index][3] > self.queue[parent_index][3]:
-                self.queue[index], self.queue[parent_index] = self.queue[parent_index], self.queue[index]
-                index = parent_index
+    # used when inserting to move the element up accordingly
+    def heapify_up(self, current_index):
+        while current_index > 0:
+            parent_index = (current_index - 1) // 2
+            if self.queue[current_index][3] > self.queue[parent_index][3]:
+                self.queue[current_index], self.queue[parent_index] = self.queue[parent_index], self.queue[current_index]
+                current_index = parent_index
             else:
                 break
     
-    def _heapify_down(self, index):
+    # used when deleting to reorganize
+    def heapify_down(self, current_index):
         while True:
-            left_child_index = 2 * index + 1
-            right_child_index = 2 * index + 2
-            largest_index = index
+            left_child_index = 2 * current_index + 1
+            right_child_index = 2 * current_index + 2
+            largest_index = current_index
             
             if left_child_index < len(self.queue) and self.queue[left_child_index][3] > self.queue[largest_index][3]:
                 largest_index = left_child_index
             if right_child_index < len(self.queue) and self.queue[right_child_index][3] > self.queue[largest_index][3]:
                 largest_index = right_child_index
             
-            if largest_index != index:
-                self.queue[index], self.queue[largest_index] = self.queue[largest_index], self.queue[index]
-                index = largest_index
+            if largest_index != current_index:
+                self.queue[current_index], self.queue[largest_index] = self.queue[largest_index], self.queue[current_index]
+                current_index = largest_index
             else:
                 break
 
-#new using bst
+# inserts from dictionary to priority queue if there are common ingredients
 def recipesort(recipe_pq, useringredients):
     sorted_recipes = {}
 
@@ -65,8 +68,7 @@ def recipesort(recipe_pq, useringredients):
     while recipe_pq.queue:
         recipename, recipeingredientlist, recipelink, priority = recipe_pq.pop()
         sorted_recipes[recipename] = (recipeingredientlist, recipelink, priority)
-
-
+    
     return sorted_recipes
 
 
@@ -84,6 +86,7 @@ def pqbutton(recipe_pq, useringredientsinput, outputtxt, elapsed_time_label):
     
     outputtxt.delete(1.0, tk.END)
 
+    # executes the sorting function
     sorted_recipes = recipesort(recipe_pq, useringredients)
     
     if sorted_recipes == []:
@@ -105,6 +108,5 @@ def pqbutton(recipe_pq, useringredientsinput, outputtxt, elapsed_time_label):
         else:
             break
 
-    
     elapsed_time = time.time() - start_time
     elapsed_time_label.config(text=f"Time taken: {elapsed_time:.3f} seconds") 
